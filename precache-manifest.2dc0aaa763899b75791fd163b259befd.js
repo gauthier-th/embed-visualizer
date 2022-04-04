52a3ef23b3ba3926e33bb941bd8a45675c75f998
gauthier-th <mail@gauthier-thomas.dev>
2022-04-04 22:02:12 +0200

    Updates

---
+++ precache-manifest.2dc0aaa763899b75791fd163b259befd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eadf99e2215146e26b2f425fd5b911a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d76641155fb394125bc8",
+    "url": "./static/css/main.20054e65.chunk.css"
+  },
+  {
+    "revision": "90e23e0b16f1fadbf4f7",
+    "url": "./static/js/2.abd7e765.chunk.js"
+  },
+  {
+    "revision": "14a886f7ea49631b50a1269c66ff1ee1",
+    "url": "./static/js/2.abd7e765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76641155fb394125bc8",
+    "url": "./static/js/main.b2d4359e.chunk.js"
+  },
+  {
+    "revision": "e7a91dd2aa196f7e5b8f54f2566cde7f",
+    "url": "./static/js/main.b2d4359e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2346e988f6b4cca9dcdc",
+    "url": "./static/js/runtime-main.694756e7.js"
+  }
+]);
